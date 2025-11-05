28b21cedde81a01e6e573afe99ae0693089be2b0
aocngocanh3108-hue <aocngocanh3108@gmail.com>
2025-11-05 13:06:32 +0700

    Cập nhật form độc giả

--- UI/frmNhapchitiettrasach.Designer.cs
+++ UI/frmNhapchitiettrasach.Designer.cs
@@ -29,21 +29,21 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            txtGhiChu = new TextBox();
+            textBox4 = new TextBox();
             label6 = new Label();
-            txtSoNgayTraTre = new TextBox();
+            textBox3 = new TextBox();
             label4 = new Label();
-            txtMaPhieuMuon = new TextBox();
-            txtTienPhat = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
             label3 = new Label();
             label5 = new Label();
-            cboTinhTrangSach = new ComboBox();
-            dtpNgayTra = new DateTimePicker();
+            comboBox1 = new ComboBox();
+            dtpNgaysinh = new DateTimePicker();
             label11 = new Label();
             label9 = new Label();
             label2 = new Label();
-            txtTenSach = new TextBox();
-            txtMaSach = new TextBox();
+            txtTendocgia = new TextBox();
+            txtMadocgia = new TextBox();
             label1 = new Label();
             btnHuy = new Button();
             btnLuu = new Button();
@@ -52,102 +52,93 @@
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(txtGhiChu);
+            groupBox1.Controls.Add(textBox4);
             groupBox1.Controls.Add(label6);
-            groupBox1.Controls.Add(txtSoNgayTraTre);
+            groupBox1.Controls.Add(textBox3);
             groupBox1.Controls.Add(label4);
-            groupBox1.Controls.Add(txtMaPhieuMuon);
-            groupBox1.Controls.Add(txtTienPhat);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
             groupBox1.Controls.Add(label3);
             groupBox1.Controls.Add(label5);
-            groupBox1.Controls.Add(cboTinhTrangSach);
-            groupBox1.Controls.Add(dtpNgayTra);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(dtpNgaysinh);
             groupBox1.Controls.Add(label11);
             groupBox1.Controls.Add(label9);
             groupBox1.Controls.Add(label2);
-            groupBox1.Controls.Add(txtTenSach);
-            groupBox1.Controls.Add(txtMaSach);
+            groupBox1.Controls.Add(txtTendocgia);
+            groupBox1.Controls.Add(txtMadocgia);
             groupBox1.Controls.Add(label1);
             groupBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 163);
-            groupBox1.Location = new Point(76, 72);
-            groupBox1.Margin = new Padding(5);
+            groupBox1.Location = new Point(47, 45);
             groupBox1.Name = "groupBox1";
-            groupBox1.Padding = new Padding(5);
-            groupBox1.Size = new Size(1172, 941);
+            groupBox1.Size = new Size(721, 588);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Thông tin trả sách";
             // 
-            // txtGhiChu
+            // textBox4
             // 
-            txtGhiChu.BorderStyle = BorderStyle.FixedSingle;
-            txtGhiChu.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
-            txtGhiChu.Location = new Point(426, 802);
-            txtGhiChu.Margin = new Padding(5);
-            txtGhiChu.Name = "txtGhiChu";
-            txtGhiChu.Size = new Size(592, 50);
-            txtGhiChu.TabIndex = 166;
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            textBox4.Location = new Point(262, 501);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(365, 34);
+            textBox4.TabIndex = 166;
             // 
             // label6
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            label6.Location = new Point(110, 806);
-            label6.Margin = new Padding(5, 0, 5, 0);
+            label6.Location = new Point(68, 504);
             label6.Name = "label6";
-            label6.Size = new Size(131, 45);
+            label6.Size = new Size(82, 28);
             label6.TabIndex = 165;
             label6.Text = "Ghi chú";
             // 
-            // txtSoNgayTraTre
+            // textBox3
             // 
-            txtSoNgayTraTre.BorderStyle = BorderStyle.FixedSingle;
-            txtSoNgayTraTre.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
-            txtSoNgayTraTre.Location = new Point(426, 482);
-            txtSoNgayTraTre.Margin = new Padding(5);
-            txtSoNgayTraTre.Name = "txtSoNgayTraTre";
-            txtSoNgayTraTre.Size = new Size(592, 50);
-            txtSoNgayTraTre.TabIndex = 164;
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            textBox3.Location = new Point(262, 301);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(365, 34);
+            textBox3.TabIndex = 164;
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            label4.Location = new Point(110, 69);
-            label4.Margin = new Padding(5, 0, 5, 0);
+            label4.Location = new Point(68, 43);
             label4.Name = "label4";
-            label4.Size = new Size(254, 45);
+            label4.Size = new Size(158, 28);
             label4.TabIndex = 163;
             label4.Text = "Mã phiếu mượn";
             // 
-            // txtMaPhieuMuon
+            // textBox2
             // 
-            txtMaPhieuMuon.BorderStyle = BorderStyle.FixedSingle;
-            txtMaPhieuMuon.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
-            txtMaPhieuMuon.Location = new Point(426, 59);
-            txtMaPhieuMuon.Margin = new Padding(5);
-            txtMaPhieuMuon.Name = "txtMaPhieuMuon";
-            txtMaPhieuMuon.Size = new Size(592, 50);
-            txtMaPhieuMuon.TabIndex = 162;
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            textBox2.Location = new Point(262, 37);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(365, 34);
+            textBox2.TabIndex = 162;
             // 
-            // txtTienPhat
+            // textBox1
             // 
-            txtTienPhat.BorderStyle = BorderStyle.FixedSingle;
-            txtTienPhat.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
-            txtTienPhat.Location = new Point(426, 696);
-            txtTienPhat.Margin = new Padding(5);
-            txtTienPhat.Name = "txtTienPhat";
-            txtTienPhat.Size = new Size(592, 50);
-            txtTienPhat.TabIndex = 161;
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            textBox1.Location = new Point(262, 435);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(365, 34);
+            textBox1.TabIndex = 161;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            label3.Location = new Point(110, 693);
-            label3.Margin = new Padding(5, 0, 5, 0);
+            label3.Location = new Point(68, 433);
             label3.Name = "label3";
-            label3.Size = new Size(197, 45);
+            label3.Size = new Size(123, 28);
             label3.TabIndex = 160;
             label3.Text = "Số tiền phạt";
             // 
@@ -155,42 +146,37 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            label5.Location = new Point(110, 589);
-            label5.Margin = new Padding(5, 0, 5, 0);
+            label5.Location = new Point(68, 368);
             label5.Name = "label5";
-            label5.Size = new Size(246, 45);
+            label5.Size = new Size(152, 28);
             label5.TabIndex = 159;
             label5.Text = "Tình trạng sách";
             // 
-            // cboTinhTrangSach
+            // comboBox1
             // 
-            cboTinhTrangSach.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
-            cboTinhTrangSach.FormattingEnabled = true;
-            cboTinhTrangSach.Location = new Point(426, 587);
-            cboTinhTrangSach.Margin = new Padding(5);
-            cboTinhTrangSach.Name = "cboTinhTrangSach";
-            cboTinhTrangSach.Size = new Size(591, 53);
-            cboTinhTrangSach.TabIndex = 158;
-            cboTinhTrangSach.SelectedIndexChanged += cboTinhTrangSach_SelectedIndexChanged_1;
+            comboBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(262, 367);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(365, 36);
+            comboBox1.TabIndex = 158;
             // 
-            // dtpNgayTra
+            // dtpNgaysinh
             // 
-            dtpNgayTra.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
-            dtpNgayTra.Format = DateTimePickerFormat.Short;
-            dtpNgayTra.Location = new Point(426, 376);
-            dtpNgayTra.Margin = new Padding(5);
-            dtpNgayTra.Name = "dtpNgayTra";
-            dtpNgayTra.Size = new Size(591, 50);
-            dtpNgayTra.TabIndex = 156;
+            dtpNgaysinh.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            dtpNgaysinh.Format = DateTimePickerFormat.Short;
+            dtpNgaysinh.Location = new Point(262, 235);
+            dtpNgaysinh.Name = "dtpNgaysinh";
+            dtpNgaysinh.Size = new Size(365, 34);
+            dtpNgaysinh.TabIndex = 156;
             // 
             // label11
             // 
             label11.AutoSize = true;
             label11.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            label11.Location = new Point(110, 173);
-            label11.Margin = new Padding(5, 0, 5, 0);
+            label11.Location = new Point(68, 108);
             label11.Name = "label11";
-            label11.Size = new Size(141, 45);
+            label11.Size = new Size(86, 28);
             label11.TabIndex = 150;
             label11.Text = "Mã sách";
             // 
@@ -198,10 +184,9 @@
             // 
             label9.AutoSize = true;
             label9.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            label9.Location = new Point(110, 485);
-            label9.Margin = new Padding(5, 0, 5, 0);
+            label9.Location = new Point(68, 303);
             label9.Name = "label9";
-            label9.Size = new Size(236, 45);
+            label9.Size = new Size(146, 28);
             label9.TabIndex = 148;
             label9.Text = "Số ngày trả trễ";
             // 
@@ -209,41 +194,37 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            label2.Location = new Point(110, 381);
-            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Location = new Point(68, 238);
             label2.Name = "label2";
-            label2.Size = new Size(147, 45);
+            label2.Size = new Size(89, 28);
             label2.TabIndex = 142;
             label2.Text = "Ngày trả";
             // 
-            // txtTenSach
+            // txtTendocgia
             // 
-            txtTenSach.BorderStyle = BorderStyle.FixedSingle;
-            txtTenSach.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
-            txtTenSach.Location = new Point(426, 270);
-            txtTenSach.Margin = new Padding(5);
-            txtTenSach.Name = "txtTenSach";
-            txtTenSach.Size = new Size(592, 50);
-            txtTenSach.TabIndex = 141;
+            txtTendocgia.BorderStyle = BorderStyle.FixedSingle;
+            txtTendocgia.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            txtTendocgia.Location = new Point(262, 169);
+            txtTendocgia.Name = "txtTendocgia";
+            txtTendocgia.Size = new Size(365, 34);
+            txtTendocgia.TabIndex = 141;
             // 
-            // txtMaSach
+            // txtMadocgia
             // 
-            txtMaSach.BorderStyle = BorderStyle.FixedSingle;
-            txtMaSach.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
-            txtMaSach.Location = new Point(426, 165);
-            txtMaSach.Margin = new Padding(5);
-            txtMaSach.Name = "txtMaSach";
-            txtMaSach.Size = new Size(592, 50);
-            txtMaSach.TabIndex = 139;
+            txtMadocgia.BorderStyle = BorderStyle.FixedSingle;
+            txtMadocgia.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+            txtMadocgia.Location = new Point(262, 103);
+            txtMadocgia.Name = "txtMadocgia";
+            txtMadocgia.Size = new Size(365, 34);
+            txtMadocgia.TabIndex = 139;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold);
-            label1.Location = new Point(110, 277);
-            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Location = new Point(68, 173);
             label1.Name = "label1";
-            label1.Size = new Size(145, 45);
+            label1.Size = new Size(90, 28);
             label1.TabIndex = 138;
             label1.Text = "Tên sách";
             // 
@@ -254,10 +235,9 @@
             btnHuy.FlatStyle = FlatStyle.Flat;
             btnHuy.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
             btnHuy.ForeColor = Color.White;
-            btnHuy.Location = new Point(871, 1053);
-            btnHuy.Margin = new Padding(5);
+            btnHuy.Location = new Point(536, 658);
             btnHuy.Name = "btnHuy";
-            btnHuy.Size = new Size(156, 61);
+            btnHuy.Size = new Size(96, 38);
             btnHuy.TabIndex = 142;
             btnHuy.Text = "Hủy";
             btnHuy.UseVisualStyleBackColor = false;
@@ -270,25 +250,22 @@
             btnLuu.FlatStyle = FlatStyle.Flat;
             btnLuu.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
             btnLuu.ForeColor = Color.White;
-            btnLuu.Location = new Point(1092, 1053);
-            btnLuu.Margin = new Padding(5);
+            btnLuu.Location = new Point(672, 658);
             btnLuu.Name = "btnLuu";
-            btnLuu.Size = new Size(156, 61);
+            btnLuu.Size = new Size(96, 38);
             btnLuu.TabIndex = 141;
             btnLuu.Text = "Lưu";
             btnLuu.UseVisualStyleBackColor = false;
-            btnLuu.Click += btnLuu_Click;
             // 
             // frmNhapchitiettrasach
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(1336, 1154);
+            ClientSize = new Size(822, 721);
             Controls.Add(btnHuy);
             Controls.Add(btnLuu);
             Controls.Add(groupBox1);
-            Margin = new Padding(5);
             Name = "frmNhapchitiettrasach";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "frmNhapchitiettrasach";
@@ -300,23 +277,23 @@
         #endregion
 
         private GroupBox groupBox1;
-        private TextBox txtTienPhat;
+        private TextBox textBox1;
         private Label label3;
         private Label label5;
-        private ComboBox cboTinhTrangSach;
-        private DateTimePicker dtpNgayTra;
+        private ComboBox comboBox1;
+        private DateTimePicker dtpNgaysinh;
         private Label label11;
         private Label label9;
         private Label label2;
-        private TextBox txtTenSach;
-        private TextBox txtMaSach;
+        private TextBox txtTendocgia;
+        private TextBox txtMadocgia;
         private Label label1;
         private Button btnHuy;
         private Button btnLuu;
         private Label label4;
-        private TextBox txtMaPhieuMuon;
-        private TextBox txtGhiChu;
+        private TextBox textBox2;
+        private TextBox textBox4;
         private Label label6;
-        private TextBox txtSoNgayTraTre;
+        private TextBox textBox3;
     }
 }
